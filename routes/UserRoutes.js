9ab333d365d66a5ab232651747c1b1626b89d7a4
Riyad Ahsan <riyad.ahsan4516@gmail.com>
2022-11-07 21:01:49 +0600

    Added update and delete user functionality

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const Router = express.Router();
 const AuthController = require("./../controllers/AuthController");
+const UserController = require("./../controllers/UserController");
 
 Router.post("/signup", AuthController.signup);
 Router.post("/login", AuthController.login);
 Router.post("/forgotPassword", AuthController.forgotpassword);
 Router.post("/resetPassword/:token", AuthController.resetPassword);
+Router.patch(
+  "/updatePassword",
+  AuthController.isloggedin,
+  AuthController.updatePassword
+);
+
+Router.patch("/updateMe", AuthController.isloggedin, UserController.updateMe);
+Router.get("/deleteMe", AuthController.isloggedin, UserController.deleteMe);
+Router.get("/", AuthController.isloggedin, UserController.getAllUsers);
 
 module.exports = Router;
