bd6ea042370b8af3ea6c330dfd6f76674eefe971
Riyad Ahsan <riyad.ahsan4516@gmail.com>
2022-11-07 21:35:35 +0600

    modified   app.js

--- app.js
+++ app.js
@@ -4,15 +4,45 @@ const CatRouter = require("./routes/CatRoutes");
 const UserRouter = require("./routes/UserRoutes");
 const AppError = require("./utils/AppError");
 const ErrorController = require("./controllers/ErrorController");
+const rateLimit = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const helmet = require("helmet");
 
 const app = express();
 
-app.use(express.json());
+app.use(helmet());
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Request limit reached. Try again after 1 hour",
+});
+
+app.use("/api", limiter);
+
+app.use(express.json({ limit: "10kb" }));
 
 if (process.env.NODE_ENV == "development") {
   console.log("Environment switched to development");
   app.use(morgan("dev"));
 }
+
+app.use(mongoSanitize());
+app.use(xss());
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
 
 app.use((req, res, next) => {
   req.requestedAt = `${new Date().toDateString()} ${new Date().toLocaleTimeString()}`;
